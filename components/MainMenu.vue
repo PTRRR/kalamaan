ed3097a18fe7104a762365c9d7a4256e7c5b641e
PTRRR <pietro.alberti@bluewin.ch>
2021-01-20 22:14:25 +0100

    Setup basic menu

---
+++ components/MainMenu.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .menu
+    .menu__home
+      a Kalamaan
+
+    .menu__sections
+      a Chi siamo
+      a Novita
+      a Contatti
+      a.menu--highlight Dona
+</template>
+
+<style lang="scss">
+.menu {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  a {
+    text-transform: uppercase;
+  }
+
+  &__sections {
+    a + a {
+      margin-left: 1rem;
+    }
+  }
+
+  &--highlight {
+    padding: 0.5rem;
+    background-color: gold;
+    color: white;
+  }
+}
+</style>
